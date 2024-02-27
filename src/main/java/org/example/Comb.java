6408c45a493fee516424197e92b322cd7484f743
b1rsuk <b1rsu_k@protonmail.ch>
2024-02-27 17:30:05 -0500

    clear imports

--- src/main/java/org/example/Comb.java
+++ src/main/java/org/example/Comb.java
@@ -3,35 +3,6 @@ package org.example;
 import java.util.Arrays;
 
 public class Comb {
-
-    public static void genPerm(int[] A, int n) {
-        int k = -1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (A[i] < A[i + 1]) {
-                k = i;
-                break;
-            }
-        }
-
-        if (k == -1) {
-            System.out.println("No more permutations available.");
-            return;
-        }
-
-        int l = -1;
-        for (int i = n - 1; i > k; i--) {
-            if (A[i] > A[k]) {
-                l = i;
-                break;
-            }
-        }
-
-        int temp = A[k];
-        A[k] = A[l];
-        A[l] = temp;
-
-        Arrays.sort(A, k + 1, n);
-    }
     public static int factorial(int n) {
         if (n == 0 || n == 1)
             return 1;
@@ -54,5 +25,4 @@ public class Comb {
     public static int _C(int n, int k) {
         return factorial(n + k - 1) / (factorial(k) * factorial(n - 1));
     }
-
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,6 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
-
-import static org.example.Comb.A;
-import static org.example.Comb.C;
-import static org.example.Comb._A;
-import static org.example.Comb._C;
-import static org.example.Comb.factorial;
 
 public class Main {
 
@@ -23,11 +16,6 @@ public class Main {
         scanner.close();
         calculate(n, k);
 
-        int[] a = generateArray(n);
-        System.out.println("\n");
-        printAllPermutations(a, n);
-        System.out.println("\n");
-        printNextPermutations(a, n);
     }
 
     private static void calculate(int n, int k) {
@@ -38,29 +26,4 @@ public class Main {
         System.out.println("Combinations with repetition: " + Comb._C(n, k));
     }
 
-    private static int[] generateArray(int n) {
-        int[] A = new int[n];
-        for (int i = 0; i < n; i++) {
-            A[i] = i + 1;
-        }
-        return A;
-    }
-
-    private static void printAllPermutations(int[] a, int n) {
-        for (int i = 1; i <= factorial(n); i++) {
-            System.out.println("Permutation " + i + ": " + Arrays.toString(a));
-            Comb.genPerm(a, n);
-        }
-    }
-
-    private static void printNextPermutations(int[] a, int n) {
-        // Виведення початкової перестановки
-        System.out.println("Permutation 1: " + Arrays.toString(a));
-
-        // Генерування та виведення наступних перестановок
-        for (int i = 2; i <= factorial(n); i++) {
-            Comb.genPerm(a, n);
-            System.out.println("Permutation " + i + ": " + Arrays.toString(a));
-        }
-    }
 }
